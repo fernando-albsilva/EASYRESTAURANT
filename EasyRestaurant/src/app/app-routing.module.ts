5bde0221304a9649f7a62e60da68b01bba073d4b
Brendow Souza <brendow.souza@sapiensia.com>
2021-07-12 08:49:45 -0300

    Adicionando o ssitema de rotas pradrao do angular para as telas de login e home.

--- EasyRestaurant/src/app/app-routing.module.ts
+++ EasyRestaurant/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageHomeComponent } from './Home/page-home-component';
+import { PageLoginComponent } from './Login/page-login-component';
 
 const routes: Routes = [
+  { path: "", component: PageLoginComponent },
   { path: 'page-home', component: PageHomeComponent }
 ];
 
