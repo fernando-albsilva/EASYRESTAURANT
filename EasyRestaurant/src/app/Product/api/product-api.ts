2b578a9ce5f327700f552d111922833ab1d75316
fernando-albsilva <fernando.albsilva@gmail.com>
2021-07-11 22:09:25 -0300

    criando componente page-product e linkando com ProductApi

--- EasyRestaurant/src/app/Product/api/product-api.ts
+++ EasyRestaurant/src/app/Product/api/product-api.ts
@@ -1,23 +1,35 @@
-import { Injectable } from "@angular/core";
-import { Observable } from 'rxjs';
+import { Injectable, OnInit } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ProductModel } from "../models/Product.model";
 
 
 
 @Injectable()
-export class ProductApi{
+export class ProductApi {
     
-public apiUrl = `https://localhost:5101/Products/GetAll`;
+    public apiUrl = `https://localhost:5101/Products/GetAll`;
 
-constructor(private http:HttpClient){
+    constructor(private http:HttpClient){}
     
-}
+    public getProducts():Array<ProductModel>{
+        let productList : Array<ProductModel> = [];
 
-getProducts(){
-    
-    this.http.get(this.apiUrl)
-    .subscribe(resultado => console.log(resultado));
+        this.http.get(this.apiUrl)
+        .subscribe((items:any) => {
 
+            let productModel = new ProductModel();
+            items.map( (item:any)=>{
+                productModel.id = item.id;
+                productModel.name = item.name;
+                productModel.unitValue = item.unitValue;
+                productModel.cost = item.cost;
+                productList.push(productModel);
+                productModel = new ProductModel();
+            });
+        });
+        return productList;
 }
 
 
